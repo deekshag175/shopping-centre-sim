3d82c7378f4c655a401e878ebc05cef467cebc39
deekshag175 <deeksha.ganta@gmail.com>
2020-12-06 15:38:22 +0000

    Added a test to check price based on power input

--- tests/test_power.py
+++ tests/test_power.py
@@ -6,3 +6,9 @@ def test_calculate_power_from_steps():
     pw = power()
     result = pw.calculatePowerFromSteps(number_of_steps)
     assert result == 0.0028
+
+def test_price_from_power():
+    power_in_kW = 1.0
+    pw = power()
+    result = pw.calculatePriceFromPower(power_in_kW)
+    assert result == 0.1437
